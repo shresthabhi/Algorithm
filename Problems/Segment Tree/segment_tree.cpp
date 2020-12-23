4dd2b0ff71ae64b4891090c9b2cc75f21d5790ee
shresthabhi <62539009+shresthabhi@users.noreply.github.com>
2020-12-23 18:24:04 +0530

    RSQ implemented

--- Problems/Segment Tree/segment_tree.cpp
+++ Problems/Segment Tree/segment_tree.cpp
@@ -9,6 +9,13 @@
 using namespace std;
 
 
+void print_arr(const int A[], int n){
+	REP(i,0,n-1){
+		cout << A[i] << " ";
+	}
+	cout << endl;
+}
+
 class SegmentTree{
 private:
 	vi st, st_sum, A;
@@ -49,19 +56,28 @@ private:
 		return (A[pl] <= A[pr]) ? pl : pr;
 	}
 
-	int rms(int p, int L, int R, int i, int j){
-		
+	int rsq(int p, int L, int R, int i, int j){
+		if(i > R || j < L) return 0;
+		if(i <= L && j >= R) return st_sum[p];
+
+		int pl_sum = rsq(left(p), L, (L+R)/2, i, j);
+		int pr_sum = rsq(right(p), (L+R)/2 + 1, R, i, j);
+
+		return pl_sum + pr_sum;
+
 	}
 public:
 	SegmentTree(const vi &_A) {
 		A = _A; 
 		n = (int)A.size();
-		st.assign(4*n, 0);
+		st.assign(2*n, 0);
+		st_sum.assign(2*n, 0);
 		build(1,0,n-1);
 
 	}
 
 	int rmq(int i, int j) { return rmq(1, 0, n-1, i, j);}
+	int rsq(int i, int j) { return rsq(1, 0, n-1, i ,j);}
 };
 
 int main(){
@@ -69,7 +85,10 @@ int main(){
 	int arr[] = {10, 2, 47, 3, 7, 9, 1, 98, 21}; 
 	vi A(arr, arr + 9);
 	SegmentTree st(A);
+	print_arr(arr, 9);
 	printf("RMQ(1, 7) = %d\n", st.rmq(1, 7));
 	printf("RMQ(3, 8) = %d\n", st.rmq(3, 8));
+	printf("RSQ(1, 7) = %d\n", st.rsq(1, 7));
+	printf("RSQ(3, 8) = %d\n", st.rsq(3, 8));
 	return 0;
 }
